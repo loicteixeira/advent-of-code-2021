0ef3478aef414923a693697908f267462dc6b2e1
Loic Teixeira <hello@lta.me>
2021-12-06 21:01:03 +0100

    Add slightly optimized version for AoC21.6

--- 06_lanternfish/part1_and_2.py
+++ 06_lanternfish/part1_and_2.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 
-from collections import Counter, defaultdict
+from collections import Counter
 from pathlib import Path
 
 DAYS_TO_GIVE_BIRTH = 7
 DAYS_TO_GIVE_FIRST_BIRTH = 9
 
 
-def get_fish_count(data, days=80):
+def get_fish_count_original(data, days=80):
     # Convert the list of fish ages to a continuous list of fish count per day
     # E.g. `"3,4,3,1,2"` becomes [0, 1, 1, 2, 1, 0, 0, 0, 0]
     fish_count_per_day = Counter(map(int, data.split(",")))
-    fish_count_per_day = defaultdict(int, fish_count_per_day.items())
     fish_count_per_day = [
-        fish_count_per_day[day] for day in range(DAYS_TO_GIVE_FIRST_BIRTH)
+        fish_count_per_day.get(day, 0) for day in range(DAYS_TO_GIVE_FIRST_BIRTH)
     ]
 
     for _ in range(days):
@@ -31,20 +30,53 @@ def get_fish_count(data, days=80):
     return sum(fish_count_per_day)
 
 
+def get_fish_count_optimised(data, days=80):
+    """
+    Same algorithm as `get_fish_count_original`,
+    but reuse the same list for each iteration (instead of creating a new one every time),
+    simply sliding the index representing "birthing" day.
+    """
+
+    # Convert the list of fish ages to a continuous list of fish count per day
+    # E.g. `"3,4,3,1,2"` becomes [0, 1, 1, 2, 1, 0, 0, 0, 0]
+    fish_count_per_day = Counter(map(int, data.split(",")))
+    fish_count_per_day = [
+        fish_count_per_day.get(day, 0) for day in range(DAYS_TO_GIVE_FIRST_BIRTH)
+    ]
+
+    for day in range(days):
+        mature_fish_idx = day % DAYS_TO_GIVE_FIRST_BIRTH
+        mature_fish_new_cycle_idx = (
+            mature_fish_idx + DAYS_TO_GIVE_BIRTH
+        ) % DAYS_TO_GIVE_FIRST_BIRTH
+
+        # Start a new cycle with the mature fish (so they mature again in 7 days)
+        # However do not reset the current day count, since it represent new births
+        # which with the sliding window already are on the right index.
+        mature_fish_count = fish_count_per_day[mature_fish_idx]
+        fish_count_per_day[mature_fish_new_cycle_idx] += mature_fish_count
+
+    return sum(fish_count_per_day)
+
+
 if __name__ == "__main__":
     example_data = "3,4,3,1,2"
     expected_result_after_18_days = 26
     expected_result_after_80_days = 5934
     expected_result_after_256_days = 26984457539
-    assert get_fish_count(example_data, 18) == expected_result_after_18_days
-    assert get_fish_count(example_data, 80) == expected_result_after_80_days
-    assert get_fish_count(example_data, 256) == expected_result_after_256_days
+    assert get_fish_count_original(example_data, 18) == expected_result_after_18_days
+    assert get_fish_count_original(example_data, 80) == expected_result_after_80_days
+    assert get_fish_count_original(example_data, 256) == expected_result_after_256_days
+
+    assert get_fish_count_optimised(example_data, 18) == expected_result_after_18_days
+    assert get_fish_count_optimised(example_data, 80) == expected_result_after_80_days
+    assert get_fish_count_optimised(example_data, 256) == expected_result_after_256_days
 
     input_file_path = Path(__file__).parent / "input.txt"
     input_data = input_file_path.read_text().splitlines()
 
-    result = get_fish_count(input_data[0], 80)
+    result = get_fish_count_optimised(input_data[0], 80)
     print(f"{result=} after 80 days")
 
-    result = get_fish_count(input_data[0], 256)
+    result = get_fish_count_optimised(input_data[0], 256)
     print(f"{result=} after 256 days")
