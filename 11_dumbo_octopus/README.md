c6db2fc77dac6aa9b67e64b8f586843c7dfa6e4b
Loic Teixeira <hello@lta.me>
2021-12-11 21:53:06 +0100

    Add challenge AoC21.11

---
+++ 11_dumbo_octopus/README.md
@@ -0,0 +1,11 @@
+# Advent of Code 2021 - Day 11: Dumbo Octopus
+
+Source: https://adventofcode.com/2021/day/11
+
+## Part 1
+
+...
+
+## Part 2
+
+...
---
+++ 11_dumbo_octopus/part2.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+from collections import Counter
+from itertools import chain, count, product
+from pathlib import Path
+
+
+def get_neighbor_coordinates(grid, r, c):
+    return (
+        (r_, c_)
+        for r_, c_ in [
+            (r - 1, c - 1),
+            (r - 1, c),
+            (r - 1, c + 1),
+            (r, c - 1),
+            (r, c + 1),
+            (r + 1, c - 1),
+            (r + 1, c),
+            (r + 1, c + 1),
+        ]
+        if 0 <= r_ < len(grid) and 0 <= c_ < len(grid[0])
+    )
+
+
+def find_synchronizing_step(data):
+    grid = [[int(cell) for cell in line] for line in data]
+    width, height = len(grid), len(grid[0])
+    coordinates = list(product(range(width), range(height)))
+
+    total_flashes = 0
+    for step in count(1):
+        step_flashes = set()
+        sub_step_flashes = set()
+
+        # Bump everybody by one
+        for r, c in coordinates:
+            grid[r][c] = (grid[r][c] + 1) % 10
+            if grid[r][c] == 0:
+                sub_step_flashes.add((r, c))
+
+        # Riplle flashes
+        while sub_step_flashes:
+            step_visited = step_flashes
+            step_flashes |= sub_step_flashes
+            prev_sub_step_flashed = sub_step_flashes
+            sub_step_flashes = set()
+
+            neighbors_to_increase = list(
+                n
+                for n in chain.from_iterable(
+                    get_neighbor_coordinates(grid, c[0], c[1])
+                    for c in prev_sub_step_flashed
+                )
+                if n not in step_visited
+            )
+
+            for (r, c), increase_amount in Counter(neighbors_to_increase).items():
+                grid[r][c] = min(grid[r][c] + increase_amount, 10) % 10
+                if grid[r][c] == 0:
+                    sub_step_flashes.add((r, c))
+
+        if len(step_flashes) == width * height:
+            return step
+
+        total_flashes += len(step_flashes)
+
+    return total_flashes
+
+
+if __name__ == "__main__":
+    example_data = [
+        "5483143223",
+        "2745854711",
+        "5264556173",
+        "6141336146",
+        "6357385478",
+        "4167524645",
+        "2176841721",
+        "6882881134",
+        "4846848554",
+        "5283751526",
+    ]
+    expected_result = 195
+    assert find_synchronizing_step(example_data) == expected_result
+
+    input_file_path = Path(__file__).parent / "input.txt"
+    input_data = input_file_path.read_text().splitlines()
+
+    result = find_synchronizing_step(input_data)
+    print(f"{result=}")
